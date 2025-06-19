70d5b4d3a2a7d1346298002c49224665bf67c014
harsh07singh <harshuusingh070@gmail.com>
2025-06-19 11:27:11 +0000

    operation

---
+++ javascript/operations.js
@@ -0,0 +1,27 @@
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "harsh"
+// let str2 = " singh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(3+2+2);
+// console.log("3"+2+2);
+// console.log(3+"2"+2);
+// console.log(3+2+"2");
+// console.log("3"+"2"+2+4);
+// console.log(3+2+"2"+"4");
+// console.log(3+"2"+"2"+4);
+
+ 
+
+
+
+
+
