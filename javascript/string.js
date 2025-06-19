bc2ea3979e5a8a68e5312f6b9dabb779397619d5
harsh07singh <harshuusingh070@gmail.com>
2025-06-19 12:11:37 +0000

    string

---
+++ javascript/string.js
@@ -0,0 +1,20 @@
+// const name ="harsh"
+// const id = 50
+
+// console.log(`Hello my name is ${name} and my id is ${id}`);
+
+const name = "  HarshSingh  "
+// console.log(name.length);
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+// console.log(name.indexOf("i"));
+// console.log(name.charAt(5));
+console.log (name.substring(0, 6));
+console.log (name.slice(0, 6));
+console.log (name.trim());
+console.log (name.replace("Singh", "Kumar"));
+// console.log(name.include("Harsh"));
+
+
+
+
