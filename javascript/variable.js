399d68b1b11335b3394c101cc5803f466b6bc68a
harsh07singh <harshuusingh070@gmail.com>
2025-06-19 07:32:21 +0000

    variables

---
+++ javascript/variable.js
@@ -0,0 +1,13 @@
+const accountId = 123456
+let accountEmail = "harshsingh@gmail.com"
+var accountPassword = "12345" 
+acountCity = "Kolkata"
+let accountState ; 
+
+accountEmail = "singhharsh@gmail.com"
+accountPassword = "54321"
+accountCity = "Asansol"
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
+
+
