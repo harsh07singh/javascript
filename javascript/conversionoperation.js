eb52993f01875d927cefda57e25c5bff43360a33
harsh07singh <harshuusingh070@gmail.com>
2025-06-19 07:30:46 +0000

    conversion of datatype

---
+++ javascript/conversionoperation.js
@@ -0,0 +1,49 @@
+// let score = 33
+// console.log(typeof score);
+// console.log(score)
+
+// let scoree = "33"
+// console.log(typeof scoree);
+// console.log(scoree);
+
+1.// //convert stringnumber into number
+
+// let valueInNumber = Number(scoree)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+
+2.//convert boolean into number
+
+// let boolean = true
+// console.log(boolean);
+// console.log(typeof boolean);
+
+// let valueInNumber = Number(boolean)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber)
+
+
+3.//convert string into number 
+//NaN not a number in value
+
+// let name = "harsh"
+// console.log(name)
+// console.log(typeof name);
+
+// let valueInNumber = Number(name)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+
+
+4.//conver number into boolean
+
+// let value = 1
+
+// let booleanvalue = Boolean(value)
+// console.log(booleanvalue)
+// console.log(typeof booleanvalue);
+
+
+5. // "" => false
+   // "harsh" => true
