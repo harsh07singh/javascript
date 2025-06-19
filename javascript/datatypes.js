8f1d7304b3bdb8cc4f11b28d43844b1a5e1b2409
harsh07singh <harshuusingh070@gmail.com>
2025-06-19 07:30:10 +0000

    datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,15 @@
+//Data types//
+
+let name = "Harsh"
+let age = 22
+let isLoggedIn = true
+
+console.log(name,age,isLoggedIn)
+
+//  number = 2 to power 53
+//  bigint
+//  string =
+//  Boolean
+//  null
+//  undefined
+//  symbol
