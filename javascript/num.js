2ea852b345daf3d74133475a7fb12e3a9224fcc4
harsh07singh <harshuusingh070@gmail.com>
2025-06-20 09:07:40 +0000

    number

---
+++ javascript/num.js
@@ -0,0 +1,47 @@
+
+//   Numbers //
+
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(4));
+
+
+// const balance2 = 23.5986
+// console.log(balance2.toPrecision(3));
+// console.log(balance2.toFixed(3));
+
+// const balance3 = 1000000
+// console.log(balance3.toLocaleString());
+// console.log(balance3.toLocaleString('en-IN'));
+
+// MIN_VALUE    MAX-VALUE    isFinite   isInfinite   //
+
+
+//Maths//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.floor(4.2));
+// console.log(Math.sqrt(15));
+// console.log(Math.min(2,3,5,6));
+// console.log(Math.max(2,3,5,8));
+// console.log(Math.trunc(20.2354));
+// console.log(Math.pow(2 , 4));
+
+
+// console.log(Math.random());
+// console.log(Math.random()*10);                              
+// console.log(Math.random()*10+1);                //to avoid 0 number
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
