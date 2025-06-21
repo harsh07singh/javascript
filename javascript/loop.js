26e06006bd962fb14808c14152870f38e8a3b17e
harsh07singh <harshuusingh070@gmail.com>
2025-06-21 16:29:13 +0000

    loop statement

---
+++ javascript/loop.js
@@ -0,0 +1,26 @@
+for(let i=0; i<=10; i++){
+    const num = i;
+    console.log(num);
+}
+
+//print table of 1 to 10
+
+for(let i=1; i<=10; i++){
+    for(let j=1; j<=10; j++){
+        console.log(i + '*' + j + '='+ i*j)
+    }
+}
+
+//3
+let Arr = ["Harsh" , "Kumar", "Singh"]
+ for(let i=0; i<Arr.length; i++){
+    const value = Arr[i];
+    console.log(value);
+ }
+ 
+//4
+ let Arr2 = [1,2,3,4,5,6]
+ for(let i=0;i<Arr2.length;i++){
+    const arrValue = Arr2[i];
+    console.log(arrValue);
+ }
