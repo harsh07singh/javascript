64a8de5caebda03fb99443b3269e6b080c040f9e
harsh07singh <harshuusingh070@gmail.com>
2025-06-21 09:31:06 +0000

    switch in javascript

---
+++ javascript/switch.js
@@ -0,0 +1,42 @@
+//formate of switch key
+//     switch(key){
+//     case value:
+//         break;
+//     default:
+//         break;
+// }
+
+
+//1 for string value 
+const month = "Mar"
+switch (month) {
+    case "Jan":
+        console.log("Jan");
+        break;
+         case "Feb":
+        console.log("Feb");
+        break;
+         case "Mar":
+        console.log("March");
+        break;
+    default:
+        console.log("No result");
+        break;
+}
+
+//2 for number value
+const num = 2
+switch (num) {
+    case 1:
+        console.log("Jan");
+        break;
+         case 2:
+        console.log("Feb");
+        break;
+         case 3:
+        console.log("March");
+        break;
+    default:
+        console.log("No result");
+        break;
+}
