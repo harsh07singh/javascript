78c706d9193f14e0bacb2fef4749686816d3aa7d
harsh07singh <harshuusingh070@gmail.com>
2025-06-21 09:37:54 +0000

    Nullish value and ternary operator

---
+++ javascript/nullish_and_ternary.js
@@ -0,0 +1,22 @@
+//Nullish Coolescing Operator (??)
+//used for null and undefined value
+let val1 
+val1 = 5??10
+console.log(val1);
+
+//2
+let val2 
+val2 = null??10
+console.log(val2);
+
+//3
+let val3 
+val3 = undefined??10
+console.log(val3);
+
+
+
+//ternary operator (?)  //to check condition in single line
+const price = 100
+price<=80? console.log("less that 80") : console.log("Greater that 80") ;
+
