d48dd2782acd7be035b7933fa564eaa3c55386c8
harsh07singh <harshuusingh070@gmail.com>
2025-06-21 05:08:31 +0000

    Diff ways to write a function

---
+++ javascript/func_in2ways.js
@@ -0,0 +1,18 @@
+function addOne(num){
+    return num+1
+}
+console.log(addOne(5));
+
+      //Or
+
+console.log(addTwo(6));
+function addTwo(num1){
+    return num1 + 2
+}      
+ 
+      // add function in variable and thn call it
+
+const addThree = function(num2){
+    return num2 + 3
+}
+console.log(addThree(3))
